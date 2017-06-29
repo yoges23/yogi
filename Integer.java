a65d619b1ada33f74d0206d791d59fa3194b3189
yoges23 <yogivpy23@gmail.com>
2017-06-29 15:12:38 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Integer 
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s=scan.next();
+int m=Integer.parseInt(s);
+System.out.print(m);
+}
+}
