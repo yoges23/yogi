7bce90334f16a5183b4b080cbe1ebae5945fdd1d
yoges23 <yogivpy23@gmail.com>
2017-09-01 16:03:55 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class power
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int m=scan.nextInt();
+int n=scan.nextInt();
+System.out.print(Math.pow(m,n));
+}
+}
