fa25a252c32f6f7664130855d9a74f9ddf0e146a
yoges23 <yogivpy23@gmail.com>
2017-09-04 10:22:12 +0530

    Update mins.java

--- mins.java
+++ mins.java
@@ -4,4 +4,19 @@ public class mins
 public static void main(String args[])
 {
 Scanner scan=new Scanner(System.in);
-int m=scan.nextInt();
+String m=scan.next();
+String n=scan.next();
+String a[]=m.split(":");
+String b[]=n.split(":");
+  int c[]=new int[a.length];
+  int d[]=new int[b.length];
+  for(int i=0;i<a.length;i++)
+  {
+    c[i]=Integer.parseInt(a[i]);
+  }
+  for(int j=0;j<b.length;j++)
+  {
+    d[i]=Integer.parseInt(b[j]);
+  }
+  
+
