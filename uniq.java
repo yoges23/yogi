e937a15d3e5845156a260ea0a7dde2ef6cc35ba7
yoges23 <yogivpy23@gmail.com>
2017-10-17 13:34:56 +0530

    Create uniq.java

---
+++ uniq.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*
+public class uniq
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int r=scan.nextInt();
+for(int i=0;i<r;i++)
+{
+System.out.println(" ");
+for(int j=0;j<=i;j++)
+{
+System.out.print("*");
+}
+}
+}}
