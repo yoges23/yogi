f8f292528f24cd0d70c642db260b154f2df8125a
yoges23 <yogivpy23@gmail.com>
2017-10-17 15:24:50 +0530

    Create tre.c

---
+++ tre.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char name={"TREE"};
+int t,b,a;
+char color[];
+printf("Thickness:");
+scanf("%d",&t);
+printf(""Branches:");
+scanf("%d",&b);
+printf("color:");
+scanf("%d",&color");
+printf("angle");
+scanf("%d",&a);
+if(t>23)
+{
+if(b>20)
+{
+if(a<=45)
+{
+printf("\n angle of the tree is:");
+}
+printf("\n branches of tree is:");
+}
+printf("\n thickness of tree is:");
+}
+getch();
+}
